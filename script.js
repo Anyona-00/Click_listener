6a8092cdba9d017b97c13e79973edf64cfd849a9
Anyona-00 <125492744+Anyona-00@users.noreply.github.com>
2023-09-10 22:47:55 +0300

    first commit

---
+++ script.js
@@ -0,0 +1,32 @@
+//selectors
+//They allow you to access and interact with specific elements on a web page.
+var title = document.getElementById("head"),
+    items = document.getElementsByClassName("list"),
+    content = document.getElementsByTagName("p");
+
+
+title.style.color = 'yellow'
+
+
+//Events
+// Events allow you to make your web pages interactive by executing code in response to user actions or other events occurring in the browser.
+var btn = document.getElementById('hit'),
+    container = document.querySelector("container");
+
+btn.addEventListener("click" , function() {
+    console.log("some one clicked me")
+});
+
+
+container.addEventListener("mouseenter", movein);
+
+function movein() {
+    console.log("The cursor has entered the container.")};
+
+document.addEventListener('keyup', function(event){
+    let keyCode = event.keyCode;
+
+  // Log the key code to the console
+  console.log("Key code: " + keyCode);
+});
+  
